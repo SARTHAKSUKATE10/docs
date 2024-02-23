f442a5371eea38c2fd8f5697cf8a64a18bb2b086
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2024-02-23 23:19:36 +0530

    props added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import { FaRegFileLines } from "react-icons/fa6";
 import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion"
 
-function Card() {
+function Card({data,reference}) {
   return (
-    <div className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+    <motion.div drag dragConstraints={reference} className='relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
        <FaRegFileLines />
-       <p className='leading-tight text-sm mt-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+       <p className='leading-tight text-sm mt-5 font-semibold'>{data.desc}</p>
        <div className='footer absolute bottom-0 w-full  left-0  '>
         <div className='flex items-center justify-between py-3 px-8 mb-3'>
-            <h5>.4mb</h5>
+            <h5>{data.fileSize}</h5>
             <span className='w-7 h-7 rounded-full bg-zinc-600 flex items-center justify-center'>
-            <LuDownload size=".7em" color='#fff'/>
-            </span>
+              {data.close?<IoClose />:<LuDownload size=".7em" color='#fff'/>}
             
+            </span>
         </div>
 
-        <div className='tag w-full py-4 bg-green-600 flex justify-center items-center'>
-          <h3 className='text-sm font-semibold'>Download Now</h3>
-        </div>
+        {data.tag.isOpen && ( <div className={`tag w-full py-4 ${data.tag.tagColor === "blue"? "bg-blue-600":"bg-green-600"} flex justify-center items-center`}>
+          <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+        </div>)}
+
+       
 
        </div>
 
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
-import Card from './Card'
+import React, { useRef } from "react";
+import Card from "./Card";
+
 
 function Foreground() {
+  const ref = useRef(null);
+  // const data=[
+  //   desc,filesize,closeOrDownload,tagDetails
+  // ];
+
+  const data = [
+    {
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+      fileSize: ".9mb",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+      fileSize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Upload",
+        tagColor: "blue",
+      },
+    },
+    {
+      desc: "This is Card on Foreground",
+      fileSize: ".9mb",
+      close: true,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    }
+  ];
+
+
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-      <Card />
-    </div>  
-  )
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
+      {data.map((item,index)=>(
+        <Card data={item} reference={ref}/>
+      ))}
+    </div>
+  );
 }
 
-export default Foreground
+export default Foreground;
