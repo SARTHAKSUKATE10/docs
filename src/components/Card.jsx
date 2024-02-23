944924324e4695d3ff439e1362f656b65a4fe1d6
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2024-02-23 09:49:41 +0530

    React icons imported

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaRegFileLines } from "react-icons/fa6";
+
+function Card() {
+  return (
+    <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>
+       <FaRegFileLines />
+
+    </div>
+  )
+}
+
+export default Card
+
