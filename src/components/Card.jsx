88ff6764ccb0bcddab4176b099a092477a2211a7
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2024-02-23 22:35:54 +0530

    Card component created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { FaRegFileLines } from "react-icons/fa6";
+import { LuDownload } from "react-icons/lu";
 
 function Card() {
   return (
-    <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>
+    <div className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
        <FaRegFileLines />
+       <p className='leading-tight text-sm mt-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+       <div className='footer absolute bottom-0 w-full  left-0  '>
+        <div className='flex items-center justify-between py-3 px-8 mb-3'>
+            <h5>.4mb</h5>
+            <span className='w-7 h-7 rounded-full bg-zinc-600 flex items-center justify-center'>
+            <LuDownload size=".7em" color='#fff'/>
+            </span>
+            
+        </div>
+
+        <div className='tag w-full py-4 bg-green-600 flex justify-center items-center'>
+          <h3 className='text-sm font-semibold'>Download Now</h3>
+        </div>
+
+       </div>
 
     </div>
   )
